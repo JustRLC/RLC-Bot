49e735f7578182cc4f2ef21950a8a368d278c9f7
RLC <RLCcoops4@gmail.com>
2016-06-14 01:04:20 +0100

    .

--- RLCBot.js
+++ RLCBot.js
@@ -39,7 +39,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me We like da, Bannan!!!");
+                    API.sendChat("/me We like da, Banana!!!");
                 }
             }
         };
